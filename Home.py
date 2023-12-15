7bf38a05210b16a0fe31ba46151ad6ce3962c7a3
flowgeniusmz <128870971+flowgeniusmz@users.noreply.github.com>
2023-12-15 15:38:58 -0600

    Update Home.py

--- Home.py
+++ Home.py
@@ -1,5 +1,22 @@
 import streamtlit as st
-from config import pagesetup
+from config import pagesetup as ps
 
 
 st.set_page_config(page_title="MagicBook Creator", page_icon="🌐☁️", layout="wide", initial_sidebar_state="collapsed")
+ps.set_title("AlmyAI", "CX")
+
+user = "Dominic Watson"
+ps.get_blue_header(f"Welcome to AlmyAI - CX {user}")
+st.markdown("Use the **tabs below** to access data and contract information")
+st.divider()
+
+tab_ExpiringContracts, tab_SendEmails, tab_AskAI = st.tabs(["Expiring Contracts", "Send Emails", "Ask AI"])
+
+with tab_ExpiringContracts:
+  ps.get_blue_header("Expiring Contracts")
+
+with tab_SendEmails:
+  ps.get_blue_header("Send Emails")
+
+with tab_AskAI:
+  ps.get_blue_header("Ask AI")
